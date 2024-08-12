b193ce30a59f12cecc6c27409dce841ee4110824
Louis Blonde <loul0uboypy@gmail.com>
2024-08-12 09:48:49 +0200

    base

---
+++ prog.py
@@ -0,0 +1,42 @@
+import random
+
+todo = input('\033[1;34mDo you want encrypt or decrypt message ? e/d ')
+
+def genPassword(length):
+    pwd = ""
+    alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123465789'
+    i = 0
+    while i < int(length):
+        pwd = pwd + random.choice(alphabet)
+        i = i + 1
+    return pwd
+def vigenere(message, key, direction=1):
+    key_index = 0
+    alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123465789'
+    encrypted_message = ''
+    for char in message:
+        if (ord(char) < 122 and ord(char) > 97) or (ord(char) > 48 and ord(char) < 57) or (ord(char) < 90 and ord(char) > 65):
+            key_char = key[key_index % len(key)]
+            key_index += 1
+            offset = alphabet.index(key_char)
+            index = alphabet.find(char)
+            new_index = (index + offset*direction) % len(alphabet)
+            encrypted_message += alphabet[new_index]
+        else:
+            encrypted_message += char
+    return encrypted_message
+def decrypt(message, key):
+    return vigenere(message, key, -1)
+def encrypt(message, key):
+    return vigenere(message, key)
+
+if todo == 'e':
+    data = input("\033[1;33mMessage to encrypt : \033")
+    hasher = genPassword(input('\033[1;36mLength security : '))
+    print(f"\033[1;92m\nKey : \033[1;35m{hasher}\n\033[1;92mEncrypted data: \033[1;35m{encrypt(data, hasher)}\033[0m")
+elif todo == 'd':
+    data = input('\033[1;33mMessage to decrypt : ')
+    hasher = input('\033[1;36mKey : ')
+    print(f'\033[1;92m\nDecrypted data : \033[1;35m{decrypt(data, hasher)}\033[0m')
+else:
+    print(f'\033[1;31mError : \'{todo}\'\033[0m')
